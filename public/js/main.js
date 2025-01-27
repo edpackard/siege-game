40d976f61c52d8fc377e6cc452429f9c9e4a70fa
edpackard <edpackard81@gmail.com>
2025-01-27 19:23:01 +0000

    log fps

--- public/js/main.js
+++ public/js/main.js
@@ -9,11 +9,15 @@ const game = new Game(gameCanvas.width, gameCanvas.height);
 let deltaTime = 0;
 let oldTimeStamp = 0;
 //let timeElapsed = 0;
+let fps = 0;
 
 const gameLoop = (timeStamp) => {
   deltaTime = (timeStamp - oldTimeStamp) / 1000;
   oldTimeStamp = timeStamp;
   //timeElapsed += deltaTime;
+
+  fps = Math.round(1 / deltaTime);
+  console.log('FPS:', fps);
 
   game.handleInputs();
   game.updateObjects();
